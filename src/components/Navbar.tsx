00ae1c7efa9c4b1bf58d51d36a2e9da27f8ca36a
namdang-fdp <namdangcoder@gmail.com>
2025-05-15 00:41:50 +0700

    feat: navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input"
+
+
+export default function Navbar() {
+  return (
+    <h1>abc</h1>
+  )
+}
