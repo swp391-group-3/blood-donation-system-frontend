45e78d3c6334d50c8ee8576ef5ce2fa6a6106ea6
namdang-fdp <namdangcoder@gmail.com>
2025-05-15 23:29:14 +0700

    feat: animation backdrop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,35 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input"
+import { motion, AnimatePresence } from "framer-motion"
+import { X, Menu, Droplet, Sun, Moon, ChevronDown, LogIn } from "lucide-react";
 
 
 export default function Navbar() {
+  const [scroll, setScroll] = useState(false)
+
   return (
-    <h1>abc</h1>
+    <header
+      className={`fixed left-0 right-0 top-0 z-50 transition-all duration-300 ${scroll ? "bg-black/70 backdrop-blur-md-py-2" : "bg-black/30 blackdrop-blur-sm py-4"}`}
+    >
+      <div className="container mx-auto px-4">
+        <div className={`flex items-center justify-between transition-all duration-300 ${scroll ? "h-14" : "h-20"}`}>
+          <a href="/" className="flex items-center gap-2 group">
+            <motion.div
+              initial={{ rotate: 0 }}
+              animate={{ rotate: [0, -10, 0, 10, 0] }}
+              transition={{ duration: 1.5, repeat: Number.POSITIVE_INFINITY, repeatDelay: 4 }}
+              className={`flex items-center justify-center rounded-full bg-gradient-to-br from-red-500 to-red-700 shadow-lg group-hover:from-red-600 group-hover:to-red-800 transition-all duration-300 ${scroll ? "h-8 w-8" : "h-10 w-10"
+                }`}
+            >
+              <Droplet
+                className={`text-white drop-shadow-md transition-all duration-300 ${scroll ? "h-5 w-5" : "h-6 w-6"}`}
+
+              />
+            </motion.div>
+          </a>
+        </div>
+      </div>
+    </header>
   )
 }
