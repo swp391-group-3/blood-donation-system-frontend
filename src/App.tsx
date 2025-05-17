c99ba494f02c4ebd050adfe05abbc3b4bb1b81cf
namdang-fdp <namdangcoder@gmail.com>
2025-05-17 15:59:55 +0700

    update: render navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Cursor from "./components/Cursor"
+import Navbar from "./components/Navbar.tsx"
 
 function App() {
 
   return (
     <div className="font-sans bg-white text-gray-900">
       <Cursor />
+      <Navbar />
     </div>
   )
 }
