acf764f4394c6420cea873e35e88aec7bd31d305
namdang-fdp <namdangcoder@gmail.com>
2025-05-17 16:10:23 +0700

    feat: scroll state and scroll effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useState, useEffect } from "react"
 import Cursor from "./components/Cursor"
 import Navbar from "./components/Navbar.tsx"
 
 function App() {
+  const [scrollY, setScrollY] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
 
   return (
     <div className="font-sans bg-white text-gray-900">
       <Cursor />
-      <Navbar />
+      <Navbar scrollY={scrollY} />
     </div>
   )
 }
