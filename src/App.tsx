d92d9419d7da011f01b8b1430c17e334260319a9
namdang-fdp <namdangcoder@gmail.com>
2025-05-24 01:37:41 +0700

    feat: render toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
+import { HeartPlus } from "lucide-react"
+import ToggleThemeButton from "@/components/ToggleThemeButton"
 import { LoginForm } from "@/components/LoginForm"
 
 export default function LoginPage() {
@@ -9,10 +9,11 @@ export default function LoginPage() {
                 <div className="flex justify-center gap-2 md:justify-start">
                     <a href="#" className="flex items-center gap-2 font-medium">
                         <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                            <GalleryVerticalEnd className="size-4" />
+                            <HeartPlus className="size-4" />
                         </div>
-                        Acme Inc.
+                        BloodBridge
                     </a>
+                    <ToggleThemeButton />
                 </div>
                 <div className="flex flex-1 items-center justify-center">
                     <div className="w-full max-w-xs">
