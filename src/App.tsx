cc0ed156c44668f6e0ca827d8ad65f703f51d28b
namdang-fdp <namdangcoder@gmail.com>
2025-05-24 23:38:44 +0700

    feat: add default register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import { Button } from '@/components/ui/button';
+import { HeartPlus } from "lucide-react"
+import ToggleThemeButton from "@/components/ToggleThemeButton"
+import { RegisterForm } from "@/components/RegisterForm"
+import { motion } from "framer-motion";
 
-const App = () => {
-    return <Button>Click me</Button>;
-};
-
-export default App;
+export default function LoginPage() {
+    return (
+        <motion.div className="grid min-h-svh lg:grid-cols-2"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+        >
+            <div className="relative hidden lg:flex items-center justify-center bg-muted">
+                <div className="relative z-10 max-w-lg text-center px-8 py-12">
+                    <h2 className="text-2xl font-bold mb-4">Welcome back!</h2>
+                    <p className="italic text-lg text-muted-foreground">
+                    “Every drop counts. Thank you for making a difference.”
+                    </p>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-between gap-2">
+                    <a href="#" className="flex items-center gap-2 font-medium">
+                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                            <HeartPlus className="size-4" />
+                        </div>
+                        BloodBridge
+                    </a>
+                    <ToggleThemeButton />
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <RegisterForm />
+                    </div>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
