d74f37c77c69caeebe901012f258adac6825615c
namdang-fdp <namdangcoder@gmail.com>
2025-05-24 13:16:05 +0700

    feat: define toggle function

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") return true;
+    if (theme === "light") return false;
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  });
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (isDarkTheme) {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDarkTheme]);
+
+  const toggleTheme = () => setIsDarkTheme((prev) => !prev);
+
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
