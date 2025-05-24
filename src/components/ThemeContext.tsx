e07f6b256e01118f125ae2f77eeea5c968cda3f7
namdang-fdp <namdangcoder@gmail.com>
2025-05-24 21:18:01 +0700

    feat: define theme context

---
+++ src/components/ThemeContext.tsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+export const ThemeContext = createContext<{
+  isDarkTheme: boolean;
+  toggleTheme: () => void;
+}>({
+  isDarkTheme: false,
+  toggleTheme: () => {},
+});
