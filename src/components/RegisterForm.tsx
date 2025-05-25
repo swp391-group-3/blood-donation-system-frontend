76dd103cac2ccd51e25c00e543363d7186e445b3
namdang-fdp <namdangcoder@gmail.com>
2025-05-25 11:59:12 +0700

    feat: implement hide/appear password

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { FcGoogle } from "react-icons/fc";
+import { PasswordInput } from "@/components/PasswordInput"
 
 export function RegisterForm({
     className,
@@ -11,9 +12,9 @@ export function RegisterForm({
     return (
         <form className={cn("flex flex-col gap-6", className)} {...props}>
             <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Login to your Account</h1>
+                <h1 className="text-2xl font-bold">Create your account</h1>
                 <p className="text-balance text-sm text-muted-foreground">
-                    Enter your email below to login to your account
+                    Enter your details below to create your account
                 </p>
             </div>
             <div className="grid gap-6">
@@ -24,17 +25,17 @@ export function RegisterForm({
                 <div className="grid gap-2">
                     <div className="flex items-center">
                         <Label htmlFor="password">Password</Label>
-                        <a
-                            href="#"
-                            className="ml-auto text-sm underline-offset-4 hover:underline"
-                        >
-                        Forgot your password?
-                        </a>
                     </div>
-                    <Input id="password" type="password" required />
+                    <PasswordInput id="password" required />
+                </div>
+                <div className="grid gap-2">
+                    <div className="flex items-center">
+                        <Label htmlFor="password">Confirm Password</Label>
+                    </div>
+                    <PasswordInput id="password" required />
                 </div>
                 <Button type="submit" className="w-full">
-                    Login
+                    Sign Up
                 </Button>
                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                     <span className="relative z-10 bg-background px-2 text-muted-foreground">
@@ -43,7 +44,7 @@ export function RegisterForm({
                 </div>
                 <Button variant="outline" className="w-full">
                     <FcGoogle className="h-5 w-5" />
-                    <span className="ml-2">Login with Google</span>
+                    <span className="ml-2">Sign up with Google</span>
                 </Button>
                 <Button variant="outline" className="w-full">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
@@ -54,13 +55,13 @@ export function RegisterForm({
                         <rect x="13" y="13" width="9" height="9" fill="#ffba08"/>
                         </g>
                     </svg>
-                    <span className="ml-2">Login with Microsoft</span>
+                    <span className="ml-2">Sign up with Microsoft</span>
                 </Button>
             </div>
             <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
+                Already have an account?{" "}
                 <a href="#" className="underline underline-offset-4">
-                    Sign up
+                    Sign in
                 </a>
             </div>
         </form>
