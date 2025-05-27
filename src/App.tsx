f6e50257e6eb6b518b3cfa3d13f055a4fbbcd6b1
namdang-fdp <namdangcoder@gmail.com>
2025-05-27 13:12:08 +0700

    feat: change layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,22 @@ export default function LoginPage() {
             animate={{ opacity: 1 }}
             transition={{ duration: 1, ease: "easeOut" }}
         >
-            <div className="relative hidden lg:flex items-center justify-center bg-muted">
-                <div className="relative z-10 max-w-lg text-center px-8 py-12">
-                    <h2 className="text-2xl font-bold mb-4">Join our community!</h2>
-                    <p className="italic text-lg text-muted-foreground">
-                    “Together we can make a difference. Every donation saves lives and brings hope to those in need.”
-                    </p>
-                </div>
-            </div>
             <div className="flex flex-col gap-4 p-6 md:p-10">
                 <div className="flex justify-between gap-2">
-                    <a href="#" className="flex items-center gap-2 font-medium">
-                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                            <HeartPlus className="size-4" />
-                        </div>
-                        BloodBridge
-                    </a>
                     <ToggleThemeButton />
                 </div>
                 <div className="flex flex-1 items-center justify-center">
                     <div className="w-full max-w-xs">
                         <RegisterForm />
                     </div>
+                </div>
+            </div>
+            <div className="relative hidden lg:flex items-center justify-center bg-muted">
+                <div className="relative z-10 max-w-lg text-center px-8 py-12">
+                    <h2 className="text-2xl font-bold mb-4">Join our community!</h2>
+                    <p className="italic text-lg text-muted-foreground">
+                    “Together we can make a difference. Every donation saves lives and brings hope to those in need.”
+                    </p>
                 </div>
             </div>
         </motion.div>
