751bfb5d6766bbfc052c134c2bcbf352102d4966
namdang-fdp <namdangcoder@gmail.com>
2025-06-03 19:27:01 +0700

    feat: add logo in sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,17 @@
+import { Heart } from "lucide-react";
 import { sidebarItems } from "./sidebar-items";
+
 
 
 export function SideBar() {
     return (
-        <div className="w-64 bg-white border-zinc-300 flex flex-col">
-
+        <div className="w-70 bg-white border-zinc-300 flex flex-col">
+            <div className="p-6 border-b border-zinc-300">
+                <div className="flex items-center space-x-2">
+                    <Heart className="h-6 w-6 text-red-600"/>
+                    <span className="text-xl font-bold text-zinc-950">BloodBridge</span>
+                </div>
+            </div>
         </div>
     );
 }
