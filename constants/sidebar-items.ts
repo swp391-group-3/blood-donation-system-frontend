5ba4029d0c98c4dd7b360e43ecf95b3c183c9f2d
namdang-fdp <namdangcoder@gmail.com>
2025-06-03 19:15:56 +0700

    feat: add constant sidebar infomation

---
+++ constants/sidebar-items.ts
@@ -0,0 +1,13 @@
+import { House, Droplet, Calendar, Activity, Award, Heart, CircleAlert, Settings } from 'lucide-react';
+
+
+export const sidebarItems = [
+    { icon: House, label: 'Dashboard', active: true },
+    { icon: Droplet, label: 'Water'},
+    { icon: Calendar, label: 'Calendar'},
+    { icon: Activity, label: 'Activity'},
+    { icon: Award, label: 'Rewards'},
+    { icon: Heart, label: 'Health'},
+    { icon: CircleAlert, label: 'Alerts'},
+    { icon: Settings, label: 'Settings'}
+];
