bfe9cadaefd8ffbaa3ec05c8a18cc730f08d875d
namdang-fdp <namdangcoder@gmail.com>
2025-06-04 16:16:33 +0700

    feat: add quick actions component

--- src/components/QuickActions.tsx
+++ src/components/QuickActions.tsx
@@ -1,7 +1,29 @@
-
-
+import { actions } from '../../constants/quick-actions';
+import { Card, CardContent } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 export function QuickActions() {
     return (
-        <h1>Hello world</h1>
+        <div>
+            <h2 className="text-lg font-semibold text-zinc-950 mb-4">Quick Actions</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {actions.map((action, index) => (
+                    <Button
+                        asChild
+                        variant="ghost"
+                        className="p-0 h-auto bg-transparent shadow-none hover:shadow-md transition-shadow text-left"
+                        key={index}
+                    >
+                        <Card className="h-full">
+                            <CardContent className="p-6 text-center flex flex-col items-center">
+                                <div className={`inline-flex p-3 rounded-full ${action.color} mb-3`}>
+                                    <action.icon className="h-6 w-6" />   
+                                </div>
+                                <h3 className="font-medium text-zinc-950">{action.title}</h3>
+                            </CardContent>
+                        </Card>
+                    </Button>
+                ))}
+            </div>
+        </div>
     )
 }
