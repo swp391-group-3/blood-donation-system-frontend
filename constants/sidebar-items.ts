000b09a9fa50ed2717bb97b2b8b0ee63c6c81862
namdang-fdp <namdangcoder@gmail.com>
2025-06-04 12:55:09 +0700

    feat: change label and add active status

--- constants/sidebar-items.ts
+++ constants/sidebar-items.ts
@@ -3,11 +3,11 @@ import { House, Droplet, Calendar, Activity, Award, Heart, CircleAlert, Settings
 
 export const sidebarItems = [
     { icon: House, label: 'Dashboard', active: true },
-    { icon: Droplet, label: 'Water'},
-    { icon: Calendar, label: 'Calendar'},
-    { icon: Activity, label: 'Activity'},
-    { icon: Award, label: 'Rewards'},
-    { icon: Heart, label: 'Health'},
-    { icon: CircleAlert, label: 'Alerts'},
-    { icon: Settings, label: 'Settings'}
+    { icon: Droplet, label: 'Blood Requests', active: false },
+    { icon: Calendar, label: 'Appointments', active: false },
+    { icon: Activity, label: 'Health Profile', active: false },
+    { icon: Award, label: 'Achievements', active: false },
+    { icon: Heart, label: 'Impact Tracker', active: false },
+    { icon: CircleAlert, label: 'Emergency Alerts', active: false },
+    { icon: Settings, label: 'Settings', active: false }
 ];
