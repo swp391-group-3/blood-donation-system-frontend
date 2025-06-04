f569a71dc3c4289113e15091981cadf9c0e7df43
namdang-fdp <namdangcoder@gmail.com>
2025-06-04 14:59:32 +0700

    feat: add sidebar border

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 export function SideBar() {
     return (
         <div className="w-70 bg-white border-zinc-300 flex flex-col">
-            <div className="p-6 border-b border-zinc-300">
+            <div className="p-6 border-b border-r border-zinc-300">
                 <div className="flex items-center space-x-2">
                     <Heart className="h-6 w-6 text-red-600" />
                     <span className="text-xl font-bold text-zinc-950">
@@ -15,7 +15,7 @@ export function SideBar() {
                 </div>
             </div>
 
-            <div className="p-4 space-y-2 flex-1">
+            <div className="p-4 space-y-2 flex-1 border-r border-zinc-300">
                 {sidebarItems.map((item, index) => (
                     <Button
                         key={index}
@@ -31,7 +31,7 @@ export function SideBar() {
                 ))}
             </div>
 
-            <div className="p-4 border-t border-zinc-300">
+            <div className="p-4 border-t border-r border-zinc-300">
                 <div className="bg-red-50  border border-red-200 rounded-lg p-4">
                     <div className="flex items-center justify-between space-x-3">
                         <div className="w-8 h-8 bg-red-100 rounded-full flex items-center justify-center">
