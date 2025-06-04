c694f3608f3b72e5d9917c18cfa0ff791f3c2e4a
namdang-fdp <namdangcoder@gmail.com>
2025-06-04 15:48:10 +0700

    feat: render quick actions section

--- src/pages/MemberHomePage.tsx
+++ src/pages/MemberHomePage.tsx
@@ -1,11 +1,13 @@
 import { HomepageLayout } from '@/components/HomepageLayout';
 import { WelcomeSection } from '@/components/WelcomeSection'
+import { QuickActions } from '@/components/QuickActions'
 
 export default function MemberHomePage() {
     return (
         <HomepageLayout>
             <div className="flex-1 space-y-6 p-6">
                 <WelcomeSection />
+                <QuickActions />
             </div>
         </HomepageLayout>
     );
