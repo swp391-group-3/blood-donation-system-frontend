0e2c7d91e9aa0eb018429a9a6b75c8b5c5762b24
namdang-fdp <namdangcoder@gmail.com>
2025-06-04 15:43:13 +0700

    feat: add welcome component

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 
 export function WelcomeSection() {
@@ -6,10 +6,21 @@ export function WelcomeSection() {
         <Card className="p-6">
             <CardContent className="flex justify-between items-center p-0">
                 <div>
-                    <div className="text-2xl font-bold mb-1">Welcome back, Nam Dang</div>
+                    <div className="text-2xl font-bold mb-1">Welcome back, Nam Dang!</div>
                     <div className="text-muted-foreground">Here's your donation dashboard</div>
                 </div>
 
+                <div className="flex gap-4 items-center">
+                    <div className="flex flex-col items-center  gap-1">
+                        <Badge variant="outline" className="bg-red-50 text-zinc-950 border-red-600 rounded-full px-3">O+</Badge>
+                        <span className="text-sm text-muted-foreground">Blood Type</span>
+                    </div>
+
+                    <div className="flex flex-col items-center  gap-1">
+                        <Badge variant="outline" className="bg-blue-100 text-zinc-950 border-blue-600 rounded-full px-3">3</Badge>
+                        <span className="text-sm text-muted-foreground">Donations</span>
+                    </div>
+                </div>
             </CardContent>
         </Card>
     )
