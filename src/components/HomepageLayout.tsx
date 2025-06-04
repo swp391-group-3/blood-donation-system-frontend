54a1d9df924a018369602e820169a86b80eb0922
namdang-fdp <namdangcoder@gmail.com>
2025-06-04 14:10:25 +0700

    feat: add homepage's header

--- src/components/HomepageLayout.tsx
+++ src/components/HomepageLayout.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { SideBar } from './SideBar';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+} from "@/components/ui/breadcrumb"
+import { Badge } from "@/components/ui/badge"
 
 interface HomepageLayoutProps {
     children: React.ReactNode;
@@ -7,11 +14,27 @@ interface HomepageLayoutProps {
 
 export function HomepageLayout({ children }: HomepageLayoutProps) {
     return (
-        <div className="flex min-h-screen">
+        <div className="flex h-screen bg-gray-50">
             <SideBar />
-            <main className="flex-1 p-6 bg-gray-100 dark:bg-gray-900">
+            <div className="flex-1 flex flex-col">
+                <header className="bg-white border-b border-zinc-300 px-6 py-4">
+                    <div className="flex items-center justify-between">
+                        <nav className="flex space-x-5">
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem>
+                                        <BreadcrumbLink className="text-zinc-950 font-semibold text-lg">DashBoard</BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                            <Badge variant="secondary">
+                                Member
+                            </Badge>
+                        </nav>
+                    </div>
+                </header>
                 {children}
-            </main>
+            </div>
         </div>
     );
 }
