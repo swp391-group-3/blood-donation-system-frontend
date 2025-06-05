9447cbd20af952ab9828d74984d539f0ccc27995
namdang-fdp <namdangcoder@gmail.com>
2025-06-05 16:44:32 +0700

    feat: add donation status card

--- src/pages/MemberHomePage.tsx
+++ src/pages/MemberHomePage.tsx
@@ -1,8 +1,10 @@
 import { HomepageLayout } from '@/components/HomepageLayout';
 import { WelcomeSection } from '@/components/WelcomeSection';
 import { actions } from '../../constants/quick-actions';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import { Progess } from '@/components/ui/progress';
+import { Droplet } from 'lucide-react';
 
 export default function MemberHomePage() {
     return (
@@ -42,6 +44,26 @@ export default function MemberHomePage() {
                         ))}
                     </div>
                 </div>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                    <Card>
+                      <CardHeader>
+                        <CardTitle className="flex items-center space-x-2">
+                          <Droplet className="w-6 h-6 stroke-red-600"></Droplet>
+                          <p className="text-2xl">Donation Status</p>
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="space-y-4">
+                        <div className="grid grid-cols-2 gap-4 text-sm">
+                          <div>
+                            <div className="text-zinc-500">Last Donation</div>
+                            <div className="font-medium text-zinc-950">1/1/2023</div>
+                          </div>
+
+                        </div>
+                      </CardContent>
+                    </Card>
+                </div>
+
             </div>
         </HomepageLayout>
     );
