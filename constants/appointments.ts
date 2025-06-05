71c2e34d295db3fc6eb849f1756ae83bbf0c6d2d
namdang-fdp <namdangcoder@gmail.com>
2025-06-05 23:45:40 +0700

    feat: add sample data for appointment

---
+++ constants/appointments.ts
@@ -0,0 +1,18 @@
+export const appointments = [
+    {
+      type: "Blood Donation",
+      location: "Central Blood Bank",
+      date: "7/10/2025",
+      time: "10:00 AM",
+      status: "Confirm",
+      statusColor: "bg-green-100 text-green-800",
+    },
+    {
+      type: "Health Check",
+      location: "Health Center",
+      date: "7/10/2025",
+      time: "10:00 AM",
+      status: "Pending",
+      statusColor: "bg-yellow-100 text-yellow-800",
+    },
+]
