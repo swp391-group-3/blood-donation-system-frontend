8f506af8c4dcd977371ece322b8518ca41acb467
namdang-fdp <namdangcoder@gmail.com>
2025-06-06 00:30:03 +0700

    feat: add sticky to sidebar to increase the UX

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -13,7 +13,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export function SideBar() {
     return (
-        <div className="w-70 bg-white border-zinc-300 flex flex-col">
+        <div className="sticky top-0 h-screen w-70 bg-white border-zinc-300 flex flex-col">
             <div className="p-6 border-b border-r border-zinc-300">
                 <div className="flex items-center space-x-2">
                     <Heart className="h-6 w-6 text-red-600" />
