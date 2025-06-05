ba622f9c5b927256213f3b96a3edf1a0e1e9ca08
namdang-fdp <namdangcoder@gmail.com>
2025-06-05 16:10:34 +0700

    feat: add children prop to homepage layout

--- src/components/HomepageLayout.tsx
+++ src/components/HomepageLayout.tsx
@@ -9,9 +9,7 @@ export function HomepageLayout({ children }: HomepageLayoutProps) {
     return (
         <div className="flex h-screen bg-gray-50">
             <SideBar />
-            <div className="flex-1 flex flex-col">
-                {children}
-            </div>
+            <div className="flex-1 flex flex-col">{children}</div>
         </div>
     );
 }
