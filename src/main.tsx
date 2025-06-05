75e879f9e00c6e5ec3da8feeb8f8aa04e8477911
namdang-fdp <namdangcoder@gmail.com>
2025-06-06 01:31:59 +0700

    feat: add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <BrowserRouter>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
     </StrictMode>,
 );
